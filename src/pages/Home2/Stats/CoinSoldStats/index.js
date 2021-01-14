517b0c26c167e1021b1d5bfa15416ccf45ac01b8
tienzlatan <39383420+tienzlatan@users.noreply.github.com>
2021-01-14 07:13:40 +0700

    + stats coin sale by day, week, month, year

--- src/pages/Home2/Stats/CoinSoldStats/index.js
+++ src/pages/Home2/Stats/CoinSoldStats/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import "./styles.css";
-import { Chart, LineAdvance } from "bizcharts";
+import { Line } from "@ant-design/charts";
 import { Card, PageHeader, Tabs, ProTable } from "antd";
 
 import { BASE_API_URL } from "../../../../utils/constant";
@@ -11,130 +11,74 @@ import AuthService from "../../../../service/auth-service";
 
 const { TabPane } = Tabs;
 
-const data = [
-  {
-    month: "Jan",
-    city: "Tokyo",
-    temperature: 7,
-  },
-  {
-    month: "Jan",
-    city: "London",
-    temperature: 3.9,
-  },
-  {
-    month: "Feb",
-    city: "Tokyo",
-    temperature: 13,
-  },
-  {
-    month: "Feb",
-    city: "London",
-    temperature: 4.2,
-  },
-  {
-    month: "Mar",
-    city: "Tokyo",
-    temperature: 16.5,
-  },
-  {
-    month: "Mar",
-    city: "London",
-    temperature: 5.7,
-  },
-  {
-    month: "Apr",
-    city: "Tokyo",
-    temperature: 14.5,
-  },
-  {
-    month: "Apr",
-    city: "London",
-    temperature: 8.5,
-  },
-  {
-    month: "May",
-    city: "Tokyo",
-    temperature: 10,
-  },
-  {
-    month: "May",
-    city: "London",
-    temperature: 11.9,
-  },
-  {
-    month: "Jun",
-    city: "Tokyo",
-    temperature: 7.5,
-  },
-  {
-    month: "Jun",
-    city: "London",
-    temperature: 15.2,
-  },
-  {
-    month: "Jul",
-    city: "Tokyo",
-    temperature: 9.2,
-  },
-  {
-    month: "Jul",
-    city: "London",
-    temperature: 17,
-  },
-  {
-    month: "Aug",
-    city: "Tokyo",
-    temperature: 14.5,
-  },
-  {
-    month: "Aug",
-    city: "London",
-    temperature: 16.6,
-  },
-  {
-    month: "Sep",
-    city: "Tokyo",
-    temperature: 9.3,
-  },
-  {
-    month: "Sep",
-    city: "London",
-    temperature: 14.2,
-  },
-  {
-    month: "Oct",
-    city: "Tokyo",
-    temperature: 8.3,
-  },
-  {
-    month: "Oct",
-    city: "London",
-    temperature: 10.3,
-  },
-  {
-    month: "Nov",
-    city: "Tokyo",
-    temperature: 8.9,
-  },
-  {
-    month: "Nov",
-    city: "London",
-    temperature: 5.6,
-  },
-  {
-    month: "Dec",
-    city: "Tokyo",
-    temperature: 5.6,
-  },
-  {
-    month: "Dec",
-    city: "London",
-    temperature: 9.8,
-  },
-];
-
 const CoinSoldStats = () => {
+  const [statsByDay, setStatsByDay] = useState([]);
+  const [statsByWeek, setStatsByWeek] = useState([]);
+  const [statsByMonth, setStatsByMonth] = useState([]);
+  const [statsByYear, setStatsByYear] = useState([]);
+
+  useEffect(() => {
+    axios(
+      `${BASE_API_URL}/api/v1/admin-stats/transactions/sale-amount-by-day`,
+      {
+        method: "GET",
+        headers: AuthService.authHeader(),
+      }
+    )
+      .then((res) => {
+        setStatsByDay(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+
+    axios(
+      `${BASE_API_URL}/api/v1/admin-stats/transactions/sale-amount-by-week`,
+      {
+        method: "GET",
+        headers: AuthService.authHeader(),
+      }
+    )
+      .then((res) => {
+        setStatsByWeek(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+
+    axios(
+      `${BASE_API_URL}/api/v1/admin-stats/transactions/sale-amount-by-month`,
+      {
+        method: "GET",
+        headers: AuthService.authHeader(),
+      }
+    )
+      .then((res) => {
+        setStatsByMonth(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+
+    axios(
+      `${BASE_API_URL}/api/v1/admin-stats/transactions/sale-amount-by-year`,
+      {
+        method: "GET",
+        headers: AuthService.authHeader(),
+      }
+    )
+      .then((res) => {
+        setStatsByYear(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  }, []);
+
   const routes = [
     {
       breadcrumbName: "Home",
@@ -146,6 +90,195 @@ const CoinSoldStats = () => {
       breadcrumbName: "Stats of Coin Sold",
     },
   ];
+
+  const configChartDay = {
+    data: statsByDay,
+
+    xField: "str_day_week",
+    yField: "total_amount",
+    label: {},
+    point: {
+      size: 5,
+      shape: "diamond",
+      style: {
+        fill: "white",
+        stroke: "#5B8FF9",
+        lineWidth: 2,
+      },
+    },
+    tooltip: {
+      showMarkers: false,
+      title: "Thống kê doanh thu bán coin trong tuần",
+      showTitle: true,
+    },
+    state: {
+      active: {
+        style: {
+          shadowColor: "yellow",
+          shadowBlur: 4,
+          stroke: "transparent",
+          fill: "red",
+        },
+      },
+    },
+    theme: {
+      geometries: {
+        point: {
+          diamond: {
+            active: {
+              style: {
+                shadowColor: "#FCEBB9",
+                shadowBlur: 2,
+                stroke: "#F6BD16",
+              },
+            },
+          },
+        },
+      },
+    },
+    interactions: [{ type: "marker-active" }],
+  };
+
+  const configChartWeek = {
+    data: statsByWeek,
+    xField: "week",
+    yField: "total_amount",
+    label: {},
+    point: {
+      size: 5,
+      shape: "diamond",
+      style: {
+        fill: "white",
+        stroke: "#5B8FF9",
+        lineWidth: 2,
+      },
+    },
+    tooltip: {
+      showMarkers: false,
+      title: "Thống kê doanh thu bán coin trong tháng",
+      showTitle: true,
+    },
+    state: {
+      active: {
+        style: {
+          shadowColor: "yellow",
+          shadowBlur: 4,
+          stroke: "transparent",
+          fill: "red",
+        },
+      },
+    },
+    theme: {
+      geometries: {
+        point: {
+          diamond: {
+            active: {
+              style: {
+                shadowColor: "#FCEBB9",
+                shadowBlur: 2,
+                stroke: "#F6BD16",
+              },
+            },
+          },
+        },
+      },
+    },
+    interactions: [{ type: "marker-active" }],
+  };
+
+  const configChartMonth = {
+    data: statsByMonth,
+    xField: "date",
+    yField: "total_amount",
+    label: {},
+    point: {
+      size: 5,
+      shape: "diamond",
+      style: {
+        fill: "white",
+        stroke: "#5B8FF9",
+        lineWidth: 2,
+      },
+    },
+    tooltip: {
+      showMarkers: false,
+      title: "Thống kê doanh thu bán coin theo tháng",
+      showTitle: true,
+    },
+    state: {
+      active: {
+        style: {
+          shadowColor: "yellow",
+          shadowBlur: 4,
+          stroke: "transparent",
+          fill: "red",
+        },
+      },
+    },
+    theme: {
+      geometries: {
+        point: {
+          diamond: {
+            active: {
+              style: {
+                shadowColor: "#FCEBB9",
+                shadowBlur: 2,
+                stroke: "#F6BD16",
+              },
+            },
+          },
+        },
+      },
+    },
+    interactions: [{ type: "marker-active" }],
+  };
+
+  const configChartYear = {
+    data: statsByYear,
+    xField: "year",
+    yField: "total_amount",
+    label: {},
+    point: {
+      size: 5,
+      shape: "diamond",
+      style: {
+        fill: "white",
+        stroke: "#5B8FF9",
+        lineWidth: 2,
+      },
+    },
+    tooltip: {
+      showMarkers: false,
+      title: "Thống kê doanh thu bán coin theo năm",
+      showTitle: true,
+    },
+    state: {
+      active: {
+        style: {
+          shadowColor: "yellow",
+          shadowBlur: 4,
+          stroke: "transparent",
+          fill: "red",
+        },
+      },
+    },
+    theme: {
+      geometries: {
+        point: {
+          diamond: {
+            active: {
+              style: {
+                shadowColor: "#FCEBB9",
+                shadowBlur: 2,
+                stroke: "#F6BD16",
+              },
+            },
+          },
+        },
+      },
+    },
+    interactions: [{ type: "marker-active" }],
+  };
 
   return (
     <HelmetProvider>
@@ -162,68 +295,16 @@ const CoinSoldStats = () => {
         <Card className="chart-card">
           <Tabs defaultActiveKey="1">
             <TabPane tab="Day" key="1">
-              <Chart
-                padding={[10, 20, 50, 40]}
-                autoFit
-                height={300}
-                data={data}
-              >
-                <LineAdvance
-                  shape="smooth"
-                  point
-                  area
-                  position="month*temperature"
-                  color="city"
-                />
-              </Chart>
+              <Line className="chart" {...configChartDay} />
             </TabPane>
             <TabPane tab="Week" key="2">
-              <Chart
-                padding={[10, 20, 50, 40]}
-                autoFit
-                height={300}
-                data={data}
-              >
-                <LineAdvance
-                  shape="smooth"
-                  point
-                  area
-                  position="month*temperature"
-                  color="city"
-                />
-              </Chart>
+              <Line className="chart" {...configChartWeek} />
             </TabPane>
             <TabPane tab="Month" key="3">
-              <Chart
-                padding={[10, 20, 50, 40]}
-                autoFit
-                height={300}
-                data={data}
-              >
-                <LineAdvance
-                  shape="smooth"
-                  point
-                  area
-                  position="month*temperature"
-                  color="city"
-                />
-              </Chart>
+              <Line className="chart" {...configChartMonth} />
             </TabPane>
             <TabPane tab="Year" key="4">
-              <Chart
-                padding={[10, 20, 50, 40]}
-                autoFit
-                height={300}
-                data={data}
-              >
-                <LineAdvance
-                  shape="smooth"
-                  point
-                  area
-                  position="month*temperature"
-                  color="city"
-                />
-              </Chart>
+              <Line className="chart" {...configChartYear} />
             </TabPane>
           </Tabs>
         </Card>
